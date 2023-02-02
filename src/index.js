ab6a58d91647e4b44937222f23f299503f93ea79
Saki <sbasken@gmail.com>
2023-02-01 17:15:47 -0800

    complete tasklister

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.querySelector("#create-task-form")
+  form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    buildToDo(e.target["new-task-description"].value)
+    form.reset();
+  })
 });
+
+function buildToDo(todo) {
+  console.log(todo)
+  let li = document.createElement('li');
+  let btn = document.createElement('Button')
+  li.textContent = `${todo}  `;
+  btn.textContent = "x";
+  li.appendChild(btn)
+  document.querySelector("#tasks").appendChild(li);
+
+  btn.addEventListener("click", handleDelete)
+  console.log(document.querySelector("ul"))
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove();
+}
+
